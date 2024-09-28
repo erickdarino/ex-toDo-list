a226923029c4225644f0a15a4899ff0f1765fcae
erickdarino <erickdarino0@gmail.com>
2024-09-28 14:47:55 -0300

    initial commit

---
+++ main.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('form').on('submit',(e)=>{
+        e.preventDefault();
+        const inputTarefa = $('#adicionar-tarefa').val();
+        const novaTarefa = $(`<li>${inputTarefa}</li>`);
+        $(novaTarefa).appendTo('ol');
+        $(novaTarefa).fadeIn(200);
+        $("#cancelar").slideDown(300)
+        $("#adicionar-tarefa").val('');
+    });
+    
+    $('ol').on('click', 'li', function(){
+        $(this).toggleClass('lista-completa');
+    });
+    $("#cancelar").on('click', function(){
+        $("li").slideUp(300, function(){
+            $(this).remove(400);
+            $("#cancelar").fadeOut(300)
+        })
+
+    })
+})
+
